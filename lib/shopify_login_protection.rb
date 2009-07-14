3b834fe4060ff3eec4c394cec96bf78607e933e5
SeniorDevCode <senior.dev.software@gmail.com>
2009-07-14 17:26:52 -0400

    Updated ShopifyAPI to have resource counting

--- lib/shopify_login_protection.rb
+++ lib/shopify_login_protection.rb
@@ -10,7 +10,7 @@ module ShopifyLoginProtection
         ActiveResource::Base.site = nil
       end
     else            
-      session[:return_to] = request.request_uri
+      session[:return_to] = request.path
       redirect_to :controller => 'login'      
     end
   end
