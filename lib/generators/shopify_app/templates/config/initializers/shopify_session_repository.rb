2f51263e5e541f6efa41d5165a5df8532e9f7a26
SeniorDevCode <senior.dev.software@gmail.com>
2015-02-20 13:25:42 -0500

    Allow override myshopify_domain from shopify_app.yml

--- lib/generators/shopify_app/templates/config/initializers/shopify_session_repository.rb
+++ lib/generators/shopify_app/templates/config/initializers/shopify_session_repository.rb
@@ -1,9 +1,8 @@
 # You should replace InMemorySessionStore with what you will be using
-# in Production. For example a model called "Shop":
-#
-# ShopifySessionRepository.storage = 'Shop'
+# in Production
 #
 # Interface to implement are self.retrieve(id) and self.store(ShopifyAPI::Session)
+#
 # Here is how you would add these functions to an ActiveRecord:
 #
 # class Shop < ActiveRecord::Base
