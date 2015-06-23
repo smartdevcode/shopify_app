5d314fd09aa75aa427cb04a4511e144a9a9ac9b5
SeniorDevCode <senior.dev.software@gmail.com>
2015-06-23 12:21:19 -0400

    fix bug in provider template

--- lib/generators/shopify_app/install/templates/shopify_provider.rb
+++ lib/generators/shopify_app/install/templates/shopify_provider.rb
@@ -2,10 +2,4 @@
     ShopifyApp.configuration.api_key,
     ShopifyApp.configuration.secret,
 
-    :scope => ShopifyApp.configuration.scope,
-
-    :setup => lambda {|env|
-       params = Rack::Utils.parse_query(env['QUERY_STRING'])
-       site_url = "https://#{params['shop']}"
-       env['omniauth.strategy'].options[:client_options][:site] = site_url
-    }
+    :scope => ShopifyApp.configuration.scope
