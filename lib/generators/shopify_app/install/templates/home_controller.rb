e6be2d866fce865372be4c5f95ce1f5c18a14f86
SeniorDevCode <senior.dev.software@gmail.com>
2015-06-24 13:51:46 -0400

    add appinfive screencast to readme under Quickstart section

--- lib/generators/shopify_app/install/templates/home_controller.rb
+++ lib/generators/shopify_app/install/templates/home_controller.rb
@@ -1,5 +1,12 @@
-class HomeController < AuthenticatedController
+class HomeController < ApplicationController
+  before_action :login_again_if_different_shop
+  around_filter :shopify_session
+<% if embedded_app? -%>
+  layout 'embedded_app'
+<% end -%>
+
   def index
     @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
   end
+
 end
