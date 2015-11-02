4d231b405ede7c30de5c5a841fd1d31d922e68a9
SeniorDevCode <senior.dev.software@gmail.com>
2015-11-02 11:41:06 -0500

    move SessionStorage from a generated class to a re-usable module

--- lib/generators/shopify_app/install/install_generator.rb
+++ lib/generators/shopify_app/install/install_generator.rb
@@ -63,7 +63,6 @@ module ShopifyApp
       def create_embedded_app_layout
         if embedded_app?
           copy_file 'embedded_app.html.erb', 'app/views/layouts/embedded_app.html.erb'
-          copy_file '_flash_messages.html.erb', 'app/views/layouts/_flash_messages.html.erb'
         end
       end
 
