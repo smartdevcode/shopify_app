395053fbb207960afde9b3df745b6252e8814271
SeniorDevCode <senior.dev.software@gmail.com>
2019-12-16 10:10:36 -0500

    Add readme instructions

--- lib/shopify_app.rb
+++ lib/shopify_app.rb
@@ -4,47 +4,35 @@ require 'shopify_app/version'
 require 'shopify_api'
 require 'omniauth-shopify-oauth2'
 
-module ShopifyApp
-  def self.rails6?
-    Rails::VERSION::MAJOR >= 6
-  end
+# config
+require 'shopify_app/configuration'
 
-  def self.use_webpacker?
-    rails6? &&
-      defined?(Webpacker) == 'constant' &&
-      !configuration.disable_webpacker
-  end
+# engine
+require 'shopify_app/engine'
 
-  # config
-  require 'shopify_app/configuration'
+# utils
+require 'shopify_app/utils'
 
-  # engine
-  require 'shopify_app/engine'
+# controller concerns
+require 'shopify_app/controller_concerns/localization'
+require 'shopify_app/controller_concerns/itp'
+require 'shopify_app/controller_concerns/login_protection'
+require 'shopify_app/controller_concerns/embedded_app'
+require 'shopify_app/controller_concerns/webhook_verification'
+require 'shopify_app/controller_concerns/app_proxy_verification'
 
-  # utils
-  require 'shopify_app/utils'
+# jobs
+require 'shopify_app/jobs/webhooks_manager_job'
+require 'shopify_app/jobs/scripttags_manager_job'
 
-  # controllers
-  require 'shopify_app/controllers/extension_verification_controller'
+# controllers
+require 'shopify_app/controllers/extension_verification_controller'
 
-  # controller concerns
-  require 'shopify_app/controller_concerns/localization'
-  require 'shopify_app/controller_concerns/itp'
-  require 'shopify_app/controller_concerns/login_protection'
-  require 'shopify_app/controller_concerns/embedded_app'
-  require 'shopify_app/controller_concerns/webhook_verification'
-  require 'shopify_app/controller_concerns/app_proxy_verification'
+# managers
+require 'shopify_app/managers/webhooks_manager'
+require 'shopify_app/managers/scripttags_manager'
 
-  # jobs
-  require 'shopify_app/jobs/webhooks_manager_job'
-  require 'shopify_app/jobs/scripttags_manager_job'
-
-  # managers
-  require 'shopify_app/managers/webhooks_manager'
-  require 'shopify_app/managers/scripttags_manager'
-
-  # session
-  require 'shopify_app/session/session_storage'
-  require 'shopify_app/session/session_repository'
-  require 'shopify_app/session/in_memory_session_store'
-end
+# session
+require 'shopify_app/session/session_storage'
+require 'shopify_app/session/session_repository'
+require 'shopify_app/session/in_memory_session_store'
